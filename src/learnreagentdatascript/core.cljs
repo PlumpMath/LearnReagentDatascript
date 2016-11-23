fcb9daf8f56e75946f464c2ef1bb5756598b4d08
Thinh P. Tran <trphthinh@gmail.com>
2016-11-23 12:13:14 +0000

    first query

--- src/learnreagentdatascript/core.cljs
+++ src/learnreagentdatascript/core.cljs
@@ -1,5 +1,6 @@
 (ns learnreagentdatascript.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [datascript.core :as d]))
 
 (enable-console-print!)
 
@@ -7,12 +8,51 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:number-users 0}))
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+;; Creates a Datascript "connection"
+;; (really an atom with the current DB value
+(def conn (d/create-conn {}))
 
-(reagent/render-component [hello-world]
+;; Add some data
+(d/transact! conn
+             [{:name "Rex" :age 3 :sex :m
+               :breed "Alsatian" :owner "Marco Polo"}
+              {:name "Sally" :age 4 :sex :f
+               :breed "Yorkshire Terrier" :owner "Mrs Pollywell"}
+              {:name "Snowy" :age 2 :sex :m
+               :breed "Wire Fox Terrier" :owner "Tintin"}
+              {:name "Same" :age 6 :sex :f
+               :breed "Basset Hound" :owner "Marco Polo"}
+              {:name "Same" :age 6 :sex :f
+               :breed "Alsatian" :owner "Guy Fawkes"}])
+
+(def q-unique-dogs '[:find ?n ?e :where [?e :name ?n]])
+
+(defn new-user! []
+  (swap! app-state update-in [:number-users] inc))
+
+(defn woofie []
+  (let [numdogs (:number-users @app-state)]
+    [:div 
+     [:header [:h1 "Welcome to Woofie"] [:p "The dog social networkd"]]
+
+     [:div {:class "site-info"}
+      [:p (str "Number of dogs on site: " (:number-users @app-state))]]
+
+     [:div {:class "user-tools"}
+      [:button {:on-click new-user!} "Register"]]
+
+     [:div {:class "members"}
+      [:h3 "Members"]
+      [:ul 
+       (map 
+        (fn [n] [:li (str (n 0))])
+        (d/q q-unique-dogs @conn))]]
+
+     ]))
+
+(reagent/render-component [woofie]
                           (. js/document (getElementById "app")))
 
 (defn on-js-reload []
